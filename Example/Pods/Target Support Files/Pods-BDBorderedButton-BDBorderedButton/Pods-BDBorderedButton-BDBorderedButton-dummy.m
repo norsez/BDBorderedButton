7020eb8dff13b9cdde5fa64c39fd7a3da367c7a5
Norsez Orankijanan <norsez@gmail.com>
2015-04-29 17:19:46 +0700

    update pod source

--- Example/Pods/Target Support Files/Pods-BDBorderedButton-BDBorderedButton/Pods-BDBorderedButton-BDBorderedButton-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BDBorderedButton_BDBorderedButton : NSObject
-@end
-@implementation PodsDummy_Pods_BDBorderedButton_BDBorderedButton
-@end
